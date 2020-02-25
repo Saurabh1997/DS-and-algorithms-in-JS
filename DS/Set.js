c693f0b6ddf8f729a681aa65c9737e070d29ff3d
Saurabh1997 <patilsaurabh.2013@gmail.com>
2020-02-25 09:04:21 +0530

    to do

--- DS/Set.js
+++ DS/Set.js
@@ -3,24 +3,50 @@
 //there's no size method in set.there's size property
 
 //union, intersection and difference of 2 set
-class mySet {
-    constructor() {
-        var collection = []
-    }
-    has = (element) => {
+function mySet() {
+    
+         var collection = []
+
+    this.hasnot = (element) => {
         return (collection.indexOf(element) == -1) // will output true if not exists
     }
-    getValues = () => {
+    this.getValues = () => {
         return console.log(collection)
     }
-    addElement = (element) => {
-        if(collection.has(element)){
+    this.addElement = (element) => {
+        if(this.hasnot(element)){
             collection.push(element)
             return true
         }
         return false
     }
+    this.removeElement = (element) => {
+        if(collection.length == 0) {
+            return false
+        }
+        else if(!collection.hasnot(element)){
+            let index = collection.indexOf(element)
+            collection.splice(index,1)
+            return true
+        }
+        return false
+    }
+
+    union = (otherSet) => {
+        var unionSet = new Set();
+        var firstSet = this.getValues();
+        var secondSet = otherSet.getValues();
+        firstSet.array.forEach(element => {
+            
+        });
+
+
+
+    }
   //  removeElement = ()
 
 
 }
+const Set = new mySet();
+Set.addElement(5);
+Set.getValues();
