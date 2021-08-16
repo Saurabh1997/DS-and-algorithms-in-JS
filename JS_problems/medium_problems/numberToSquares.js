692da292c3531b6440c758c38a87157a5cf33b34
Saurabh1997 <patilsaurabh.2013@gmail.com>
2021-08-16 17:59:17 +0530

    numbertoSquaresProb

--- JS_problems/medium_problems/numberToSquares.js
+++ JS_problems/medium_problems/numberToSquares.js
@@ -1,21 +1,40 @@
 //  O (N**2)  - complexity as it array.indexOf also has O(N). so Nested loop
+
+// const isSquares = (nums, squrs) => {
+//     if(nums.length !== squrs.length) return false
+//     for(let i = 0;i< nums.length;i++){
+//         let sqrs = squrs.indexOf(nums[i] ** 2)
+//         if(sqrs === -1){
+//             return false
+//         }
+//         squrs.splice(sqrs,1)
+//         console.log("hello",sqrs,squrs)
+//     }
+//     return true
+// }
+
 const isSquares = (nums, squrs) => {
-    if(nums.length !== squrs.length) return false
-    for(let i = 0;i< nums.length;i++){
-        let sqrs = squrs.indexOf(nums[i] ** 2)
-        if(sqrs === -1){
-            return false
-        }
-        squrs.splice(sqrs,1)
-        console.log("hello",sqrs,squrs)
-    }
-    return true    
-}
+  if (nums.length !== squrs.length) return false;
+  let numFreq = {},
+    sqFreq = {};
 
+  for (num of nums) {
+    numFreq[num] = numFreq[num] + 1 || 1;
+  }
+  console.log(numFreq);
 
+  for (sqrs of squrs) {
+    sqFreq[sqrs] = sqFreq[sqrs] + 1 || 1;
+  }
+  console.log(sqFreq);
 
+  for (num in numFreq) {
+    if (sqFreq[num ** 2] != numFreq[num]) return false;
+  }
+  return true;
+};
 
-let nums = [1,3,2] // can be repeating
-let squrs = [1,4,9] // can be repeating 
+let nums = [1, 2, 3]; // can be repeating
+let squrs = [1, 4, 9]; // can be repeating
 
-console.log(isSquares(nums,squrs), "Equal")
+console.log(isSquares(nums, squrs), "Equal");
