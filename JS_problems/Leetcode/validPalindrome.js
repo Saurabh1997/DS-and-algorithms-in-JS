4dd1a461e3e7c6def612b70ad2979bb3e60e9da3
sashokpatil <sashokpatil@deloitte.com>
2024-05-24 10:05:36 +0530

    adding 1 solved problem

---
+++ JS_problems/Leetcode/validPalindrome.js
@@ -0,0 +1,9 @@
+const validPalindrome = (str) => {};
+
+console.log(
+  " result ",
+  validPalindrome("A man, a plan, a canal: Panama"),
+  " s ",
+
+  /^[a-zA-Z0-9]+$/.test("str 23")
+);
--- JS_problems/hackerrank/countValleys.js
+++ JS_problems/hackerrank/countValleys.js
@@ -4,25 +4,17 @@
 // Sample Output
 // 1
 // D - downhill, U - uphill. sealevel - 0
-
 function countingValleys(steps, path) {
-  // Write your code here
   let units = 0; // sea level
-  let started = false;
-  //   let ended = false;
   let valleyCount = 0;
   for (let i = 0; i < steps; i++) {
     if (path[i] === "U") {
-      if (units === 0 && started) {
-        started = false;
+      units++;
+      if (units === 0) {
         valleyCount++;
       }
-      units++;
     }
     if (path[i] === "D") {
-      if (units === 0 && !started) {
-        started = true;
-      }
       units--;
     }
   }
@@ -30,4 +22,4 @@ function countingValleys(steps, path) {
   return valleyCount;
 }
 
-console.log(" data ", countingValleys(12, "DDUUDDUDUUUD"));
+console.log(" result =  ", countingValleys(8, "UDDDUDUU"));
